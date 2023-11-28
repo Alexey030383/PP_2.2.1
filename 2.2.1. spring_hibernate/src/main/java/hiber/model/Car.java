0a5ad6f7a172c911d0f3a76e0172cc3dee4b6b9c
skillbox43 <for_leha@mail.ru>
2023-11-29 00:24:35 +0300

    PP 2.2.1 is done

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -62,24 +62,21 @@ public class Car {
     }
 
     @Override
-    public String toString() {
-        return "Car {" +
-                "id=" + id +
-                ", model='" + model + '\'' +
-                ", series=" + series +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return Objects.equals(id, car.id) && Objects.equals(model, car.model) && Objects.equals(series, car.series);
+        return series == car.series && Objects.equals(id, car.id) &&
+                Objects.equals(model, car.model) && Objects.equals(user, car.user);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, model, series);
+        return Objects.hash(id, model, series, user);
+    }
+
+    @Override
+    public String toString() {
+        return "Car model = " + model + " series = " + series + "\n";
     }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -76,28 +76,26 @@ public class User {
     }
 
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", car=" + car +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName)
-                && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email)
-                && Objects.equals(car, user.car);
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) &&
+                Objects.equals(car, user.car);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, firstName, lastName, email, car);
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id + '\n' +
+                "firstName = " + firstName + '\n' +
+                "lastName = " + lastName + '\n' +
+                "email = " + email + '\n' +
+                car.toString();
     }
 }
