4c5c5bb907278f7b84d3a365672afec5e28de397
skillbox43 <for_leha@mail.ru>
2023-12-01 00:23:21 +0300

    PP 2.2.1 corrected

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,6 +2,7 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
@@ -12,7 +13,7 @@ public class UserDaoImp implements UserDao {
 
     private final SessionFactory sessionFactory;
 
-
+    @Autowired
     public UserDaoImp(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
